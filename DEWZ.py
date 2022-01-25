95d16fcec7e40f447b788fcebfd73abe4e6e640f
GKR <95577552+GKRBROS@users.noreply.github.com>
2022-01-25 12:08:41 +0000

    bot 1st working

--- DEWZ.py
+++ DEWZ.py
@@ -8,12 +8,13 @@ async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_msg(message):
+async def on_message(message):
   if message.author == client.user:
     return
 
   if message.content.startswith('/news'):
     await message.channel.send('Hallo')
+    print('hallo')
 
 my_secret = os.environ['token']
 client.run(my_secret)
