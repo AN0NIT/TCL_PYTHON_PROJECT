6fc7db074a546f2c09e9d5d7563b3b17327ef24f
GKR <95577552+GKRBROS@users.noreply.github.com>
2022-01-25 11:28:24 +0000

    edits

--- DEWZ.py
+++ DEWZ.py
@@ -1,1 +1,20 @@
+import os
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_msg(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('/news'):
+    await message.channel.send('Hallo')
+
+my_secret = os.environ['token']
+client.run(my_secret)
+
