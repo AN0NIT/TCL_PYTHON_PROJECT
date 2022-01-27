2356ff972ddb09e42f82e693aaa3072db008a063
Nandana <97794941+miss-das@users.noreply.github.com>
2022-01-27 17:00:18 +0000

    entertainment

--- DEWZ.py
+++ DEWZ.py
@@ -19,14 +19,19 @@ async def on_message(message):
     await message.channel.send('which type of news would u prefer :1.tech&sci, 2.health&sports, 3.film&music [include / and numer of the topic]')
    
   if message.content.startswith('/1'):
-    clients = gnewsclient.NewsClient(language='english',location='world',topic='technology and science')
-    news_list = clients.get_news()
-    #print(news_list)
+    clients = gnewsclient.NewsClient(language='english',location='world',topic='technology')
+    news_list = clients.get_news()    
     for i in range(5):
-      item=news_list[i]
-      #msg=str(item['title'],item['link'])  
+      item=news_list[i]      
       await message.channel.send(item['link'])
-      
+
+  if message.content.startswith('/3'):
+    clientss = gnewsclient.NewsClient(language='english',  location='world',topic='entertainment')
+    news_lists = clientss.get_news()   
+    print(news_lists)
+    for i in range(5):
+      items=news_lists[i]        
+      await message.channel.send(items['link'])    
 
 
 
