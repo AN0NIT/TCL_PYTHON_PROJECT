ee50d1c7925aafceb17851ca8767fa6ec36460d7
GKR <95577552+GKRBROS@users.noreply.github.com>
2022-01-28 04:00:51 +0000

    keep alive

--- DEWZ.py
+++ DEWZ.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from gnewsclient import gnewsclient
-
+import keep_alive
 
 client = discord.Client()
 
@@ -16,7 +16,7 @@ async def on_message(message):
   
   
   if message.content.startswith('/news'):
-    await message.channel.send('which type of news would u prefer :1.tech&sci, 2.health&sports, 3.film&music [include / and numer of the topic]')
+    await message.channel.send('which type of news would u prefer :1.Technology, 2.Health, 3.Entertainment, 4.Sports, 5.Science [include / and numer of the topic]')
    
   if message.content.startswith('/1'):
     clients = gnewsclient.NewsClient(language='english',location='world',topic='technology')
@@ -31,10 +31,23 @@ async def on_message(message):
     print(news_lists)
     for i in range(5):
       items=news_lists[i]        
-      await message.channel.send(items['link'])    
+      await message.channel.send(items['link'])
 
+  if message.content.startswith('/4'):
+    clients = gnewsclient.NewsClient(language='english',location='world',topic='sports')
+    news_list = clients.get_news()    
+    for i in range(5):
+      item=news_list[i]      
+      await message.channel.send(item['link'])
 
+  if message.content.startswith('/5'):
+    clients = gnewsclient.NewsClient(language='english',location='world',topic='science')
+    news_list = clients.get_news()    
+    for i in range(5):
+      item=news_list[i]      
+      await message.channel.send(item['link']) 
 
+keep_alive.keep_alive()
 my_secret = os.environ['token']
 client.run(my_secret)
 
